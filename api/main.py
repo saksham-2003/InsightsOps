87b06dd9e21f51def2c5e5d11897aa0f9109736c
saksham-2003 <sakshamlakhani1906@gmail.com>
2026-08-26 23:22:28 +0530

    Allow production frontend in CORS

--- api/main.py
+++ api/main.py
@@ -32,7 +32,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://insightsops-1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
